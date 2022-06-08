058fc6f9430e7223ce4e4794dd685beaf1f2fc1d
Igor Tozetti <igorct77@gmail.com>
2022-06-07 21:45:39 -0300

    db_product

--- projeto-final/db/db_store.sql
+++ projeto-final/db/db_store.sql
@@ -10,13 +10,23 @@ CREATE TABLE tb_category (
     name VARCHAR(30) NOT NULL,
     description VARCHAR(100) NOT NULL
 );
+CREATE TABLE tb_product (
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    photo VARCHAR(255)  NOT NULL,
+    price FLOAT(5,2) NOT NULL,
+    amount INT(5) NOT NULL,
+    -- tipo de dado que salva data e hora --
+    created_at DATETIME NOT NULL,
+    categoria_id INT(11) NOT NULL
+);
 
 
--- Inserir
+-- Inserir na tabela de categoria
 
 INSERT INTO tb_category (name, description)
 VALUES
 ('Informática', 'Produtos de Informática e acessórios para computador'),
 ('Escritório', 'Canetas, cadernos, folhas, etc'),
 ('Eletrônicos', 'TVs, Som portátil, Caixas de som');
-
