d90a6fd7c7216e98eaf9f9bb41636d563649300f
Igor Tozetti <igorct77@gmail.com>
2022-06-07 22:31:16 -0300

    connection php/bd

--- projeto-final/public/index.php
+++ projeto-final/public/index.php
@@ -2,36 +2,52 @@
 ini_set('display_errors', 1);
 include '../vendor/autoload.php';
 
-use App\Controller\IndexController;
-use App\Controller\ProductController;
-use App\Controller\CategoryController;
-use App\Controller\ErrorController;
+$database = 'db_store';
+$username = 'root';
+$password = 'pass1234';
+// conexão do banco de dados com o php, utilizando nome,  login e senha do bd
+$connection = new PDO('mysql:host=localhost;dbname='.$database, $username, $password);
+$query = 'SELECT * FROM tb_category';
 
-$url = explode("?",  $_SERVER['REQUEST_URI'])[0];
 
-function createRoute(string $controllerName, string $methodName) {
-    return [
-        'controller' => $controllerName,
-        'method' => $methodName,
-    ];
-}
-$routes = [
-    '/' => createRoute(IndexController::class, 'indexAction'),
-    '/produtos' => createRoute(ProductController::class, 'listAction'),
-    '/categoria' => createRoute(CategoryController::class, 'listAction'),
+$preparacao = $connection->prepare($query);
+$preparacao->execute();
 
-];
+var_dump($preparacao->fetch());
 
-if(false === isset($routes[$url])) {
-    // $e = new ErrorController();
-    // $e->notFoundAction();
-    ( new ErrorController() )->notFoundAction();
-    exit;
-}
-$controllerName = $routes[$url]['controller'];
-$methodName = $routes[$url]['method'];
+// while ($registros = $preparacao->fetch()) {
+//     var_dump($registros);
+// }
+// use App\Controller\IndexController;
+// use App\Controller\ProductController;
+// use App\Controller\CategoryController;
+// use App\Controller\ErrorController;
 
-(new $controllerName())->$methodName();
+// $url = explode("?",  $_SERVER['REQUEST_URI'])[0];
+
+// function createRoute(string $controllerName, string $methodName) {
+//     return [
+//         'controller' => $controllerName,
+//         'method' => $methodName,
+//     ];
+// }
+// $routes = [
+//     '/' => createRoute(IndexController::class, 'indexAction'),
+//     '/produtos' => createRoute(ProductController::class, 'listAction'),
+//     '/categoria' => createRoute(CategoryController::class, 'listAction'),
+
+// ];
+
+// if(false === isset($routes[$url])) {
+//     // $e = new ErrorController();
+//     // $e->notFoundAction();
+//     ( new ErrorController() )->notFoundAction();
+//     exit;
+// }
+// $controllerName = $routes[$url]['controller'];
+// $methodName = $routes[$url]['method'];
+
+// (new $controllerName())->$methodName();
 
 
 // $c = new IndexController();
